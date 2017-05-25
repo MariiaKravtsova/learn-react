03f97973454598c41aac958e31b8b85e9a953028
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-05-24 22:13:24 -0400

    Add the blueprint

--- hi-app/src/App.js
+++ hi-app/src/App.js
@@ -1,8 +1,52 @@
 import React from 'react';
 
+const Stars = (props) => {
+  return (
+    <div>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button>=</button>
+    </div>
+  )
+}
+
+const Answer = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Play Nine</h3>
+        <Stars />
+        <Button />
+        <Answer />
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}!</h1>;
+    return (
+    <div>
+      <Game />
+    </div>
+    )
   }
 }
 

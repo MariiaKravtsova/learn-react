362a602eee9df16cbd3ad3f0bc8976e9aeecad44
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-23 18:11:31 -0500

    Add a whole bunch of magic

--- react-redux/book-list/src/containers/book-list.js
+++ react-redux/book-list/src/containers/book-list.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { selectBook } from '../actions/index'
+import { bindActionCreators } from 'redux'
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
-      return <li className='list-group-item' key={book.title}>{book.title}</li>
+      return (
+        <li className="list-group-item" key={book.title} onClick={() => this.props.selectBook(book)}>
+          {book.title}
+        </li>
+      )
     })
   }
 
   render() {
-    return (
-      <ul className='list-group col-sm-4'>
-        {this.renderList()}
-      </ul>
-    )
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>
   }
 }
 
@@ -23,4 +25,8 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList)
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
--- react-redux/book-list/src/reducers/reducer_books.js
+++ react-redux/book-list/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'If Hemingway Wrote JavaScript' },
-    { title: 'Learn Ruby the Hard Way' },
-    { title: 'Effective Programming: More Than Writing Code' },
-    { title: 'Loren Ipsum' },
+    { title: 'If Hemingway Wrote JavaScript', pages: 333 },
+    { title: 'Learn Ruby the Hard Way', pages: 233 },
+    { title: 'Effective Programming: More Than Writing Code', pages: 456 },
+    { title: 'Loren Ipsum', pages: 123 },
   ]
 }

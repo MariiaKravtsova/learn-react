fb8cd6c8f0c296d99723b82a4af1af489b55d8d7
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-23 17:08:27 -0500

    Create a container

---
+++ react-redux/book-list/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'If Hemingway Wrote JavaScript' },
+    { title: 'Learn Ruby the Hard Way' },
+    { title: 'Effective Programming: More Than Writing Code' },
+    { title: 'Loren Ipsum' },
+  ]
+}

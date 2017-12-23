a92a1031a9ac4bbdd8a4b8bd229657b5627823ce
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-23 16:06:49 -0500

    Finish the React part of the youtube-like app

--- react-redux/ReduxSimpleStarter/src/components/app.js
+++ react-redux/ReduxSimpleStarter/src/components/app.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react'
 import SearchBar from './search-bar'
 import YTSearch from 'youtube-api-search'
+import VideoList from './video-list'
+import VideoDetail from './video-detail'
 import { API_KEY } from '../../config'
+import { debounce } from 'lodash'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       videos: [],
+       selectedVideo: null
+    }
 
+    this.videoSearch('cats')
+  }
+  
+  videoSearch = (term) => YTSearch({key: API_KEY, term}, (videos) => this.setState({ videos, selectedVideo: videos[0] }))
 
-  getVideos = () => YTSearch({key: API_KEY, term: 'cats'}, (data) => console.log(data))
-
+  
   render() {
+    const videoSearch = debounce(term => { this.videoSearch(term) }, 300)
+
     return (
       <div>
-      <SearchBar />
-        React simple starter</div>
+        <SearchBar onSearchTerm={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        />
+      </div>
     );
   }
 }

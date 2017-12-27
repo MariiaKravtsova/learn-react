dd21ff7e3e309596f70dc7068fb752bc1703495c
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-26 22:59:53 -0500

    Start the weather forecast

---
+++ react-redux/weather/src/containers/search-bar.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      term: ''
+    }
+  }
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value })
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          placeholder="Get a five day forecast"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
+        </span>
+      </form>
+    )
+  }
+}

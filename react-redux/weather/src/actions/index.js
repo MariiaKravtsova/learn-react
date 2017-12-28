018c7630590a157208118a2862d3ea2acac19b33
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-27 22:05:57 -0500

    Create action to fetch weather from open weather map

--- react-redux/weather/src/actions/index.js
+++ react-redux/weather/src/actions/index.js
@@ -1,2 +1,13 @@
 import { API_KEY } from '../../config'
+import { FETCH_WEATHER } from './types'
+import axios from 'axios'
 
+export function fetchWeather(city) {
+  const url = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&q=${city},us`
+  const request = axios.get(url)
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  }
+}

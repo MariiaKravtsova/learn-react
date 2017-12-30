6fd10cc630da6654c5337848f01b0f4eec11e9ef
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2017-12-30 18:52:59 -0500

    Get new starter

---
+++ react-redux/blog/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom'
+import PostIndex from './components/posts-index'
+
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <Route path='/' exact component={PostIndex} />
+    </BrowserRouter>
+  </Provider>
+  , document.querySelector('.container'));

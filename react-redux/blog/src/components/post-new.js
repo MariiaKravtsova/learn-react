af6290946a91a9572d11703b58985b80627846a6
Mariia Kravtsova <kravtsova.mariia@gmail.com>
2018-01-01 15:41:00 -0500

    Add redux form, it's pretty cool

--- react-redux/blog/src/components/post-new.js
+++ react-redux/blog/src/components/post-new.js
@@ -1,11 +1,73 @@
 import React, { Component } from 'react'
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
 
-export default class PostNew extends Component {
-  render() {
+class PostNew extends Component {
+  renderField = (field) => {
+    const { touched, error } = field.meta
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div>
-        new
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className='form-control' {...field.input} />
+        <div className='text-help'>
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
+
+  onSubmit = (values) => {
+    console.log(values)
+  } 
+
+  render() {
+    const { handleSubmit } = this.props
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <Field
+          label='Title'
+          name='title'
+          component={this.renderField}
+        />
+        <Field
+          label='Categories'
+          name='categories'
+          component={this.renderField}
+        />
+        <Field
+          label='Contents'
+          name='contents'
+          component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'>Submit</button>
+        <Link className='btn btn-danger' to='/' style={{margin: '10px'}}>Cancel</Link>
+      </form>
+    )
+  }
 }
+
+function validate(values) {
+  const errors = {}
+
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+
+  if (!values.categories) {
+    errors.categories = 'Enter a category.'
+  }
+
+  if (!values.contents) {
+    errors.contents = 'Some content required.'
+  }
+
+  return errors
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm'
+})(PostNew)
